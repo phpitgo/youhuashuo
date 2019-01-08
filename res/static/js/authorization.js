44aafc424179e23d3eca0c1613d6881baf55440c
刘海亮 <you@example.com>
2019-01-08 13:42:24 +0800

    ajaxSetup authorization http status 401

--- res/static/js/authorization.js
+++ res/static/js/authorization.js
@@ -1,9 +1,10 @@
-layui.define(['jquery'], function(exports){ //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
-	var $ = layui.jquery;
+layui.define(['jquery', 'layer'], function(exports){ //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
+	var $ = layui.jquery
+	,layer = layui.layer;
 	$.ajaxSetup({error:function(XMLHttpRequest, textStatus, errorThrown) {
-        	console.log(XMLHttpRequest.status);
-        	console.log(XMLHttpRequest.readyState);
-        	console.log(textStatus);
+        	if(XMLHttpRequest.status == 401){
+        		layer.msg(XMLHttpRequest.responseJSON.message)
+        	}
     	},
 	});
 	exports('authorization', null); 
